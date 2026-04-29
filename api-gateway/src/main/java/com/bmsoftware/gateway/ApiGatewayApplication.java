b09f5f6c0ea7884b1b9f92bf4d00eeb996ba8ec5
BogdanM <bogdan.materatskyy@gmail.com>
2026-04-29 21:17:44 +0300

    pps-7: add global rate limiting to API Gateway using Bucket4j
    #7

--- api-gateway/src/main/java/com/bmsoftware/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bmsoftware/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.bmsoftware.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "com.bmsoftware")
+@EnableCaching
 public class ApiGatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(ApiGatewayApplication.class, args);
